fd97249c9b4df41bdfa891356b899fda59cb856a
Peter Buttgig <vinniethepooloo@protonmail.com>
2020-07-19 21:09:31 +0500

    set up a skeleton

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.scss';
+
+export function App() {
+  return (
+    <>
+      <header>
+        <h1>Header</h1>
+        <nav>Nav bar</nav>
+      </header>
+      <main>
+        <h1>Main</h1>
+        <section>Content</section>
+        <aside>Aside</aside>
+      </main>
+      <footer>
+        <h1>Footer</h1>
+        <address>Contact info</address>
+      </footer>
+    </>
+  );
+}
