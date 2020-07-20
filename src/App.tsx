2eac93138ed4ac0d7b9aa3f1610ccb1de03e2a81
Peter Buttgig <vinniethepooloo@protonmail.com>
2020-07-20 20:37:39 +0500

    fluffed up with css

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,49 @@
 import React from 'react';
 import './App.scss';
+import { sunflowerMandala } from './img/imgObjects';
+import { Main } from './Main';
 
 export function App() {
   return (
     <>
-      <header>
-        <h1>Header</h1>
-        <nav>Nav bar</nav>
-      </header>
-      <main>
-        <h1>Main</h1>
-        <section>Content</section>
-        <aside>Aside</aside>
-      </main>
+      <Header />
+      <Main />
       <footer>
-        <h1>Footer</h1>
-        <address>Contact info</address>
+        <address>
+          <p>This site was created by Pete Buttgig</p>
+          <p>
+            E-mail: <a href="mailto: vinniethepooloo@protonmail.com">vinniethepoolooATprotonmail.com</a>
+          </p>
+        </address>
       </footer>
     </>
   );
 }
+
+function Header() {
+  return (
+    <header>
+        <h1>
+          Peter's corner 
+          <img src={sunflowerMandala.png} alt="sunflower logo" />
+        </h1>
+        <small>The application is running in <b>{process.env.NODE_ENV}</b> mode.</small>
+        <nav>
+          {/* <h2>Navigation</h2> */}
+          <ul>
+            <li><a href="#">Section 1</a></li>
+            <li><a href="#">Section 2</a></li>
+            <li><a href="#">Section 3</a></li>
+            <li><a href="#">Section 4</a></li>
+            <li><a href="#">Section 5</a></li>
+            <li><a href="#">Section 6</a></li>
+            <li><a href="#">Section 7</a></li>
+            <li><a href="#">Section 8</a></li>
+            <li><a href="#">Section 9</a></li>
+            <li><a href="#">Section 10</a></li>
+          </ul>
+        </nav>
+      </header>
+  );
+}
+
