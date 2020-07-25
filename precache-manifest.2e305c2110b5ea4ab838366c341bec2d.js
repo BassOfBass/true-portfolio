55ecd92608facdd5374dda26f1ecff2524fb6948
Peter Buttgig <vinniethepooloo@protonmail.com>
2020-07-25 11:09:48 +0500

    Updates

---
+++ precache-manifest.2e305c2110b5ea4ab838366c341bec2d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f2f4464d5368950c6c96c83128c779",
+    "url": "/true-portfolio/index.html"
+  },
+  {
+    "revision": "067d85d5562f66047d4c",
+    "url": "/true-portfolio/static/css/main.69f5edb8.chunk.css"
+  },
+  {
+    "revision": "b94510e0003360037782",
+    "url": "/true-portfolio/static/js/2.a9c7c381.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/true-portfolio/static/js/2.a9c7c381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "067d85d5562f66047d4c",
+    "url": "/true-portfolio/static/js/main.23ff2e2d.chunk.js"
+  },
+  {
+    "revision": "a7953590627ef368e979",
+    "url": "/true-portfolio/static/js/runtime-main.4e173fbb.js"
+  },
+  {
+    "revision": "e7eed119a5a4742e88f9d49c679b85b5",
+    "url": "/true-portfolio/static/media/00091A Floral.e7eed119.png"
+  },
+  {
+    "revision": "de4fa0602a71551166b580423817a251",
+    "url": "/true-portfolio/static/media/00091B Floral.de4fa060.png"
+  },
+  {
+    "revision": "f112751c0d437dd4bc658f3055fdd8f7",
+    "url": "/true-portfolio/static/media/01376 Sunflower Mandala.f112751c.png"
+  }
+]);
